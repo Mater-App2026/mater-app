c1e949467fa18bcb39e5f8b05bcd5e5ce6477c07
Mater-App2026 <jorgerubido@gmail.com>
2026-07-01 23:03:26 -0400

    Implement service worker registration
    Add service worker registration to improve offline capabilities.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(() => console.log('Service worker registrado correctamente'))
+      .catch((error) => console.log('Error registrando service worker:', error));
+  });
+}
